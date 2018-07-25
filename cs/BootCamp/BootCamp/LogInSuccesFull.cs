420a8de66e6229b888d86e061098b17d73bd89bf
lianklaver <lianne.klaver@polteq.com>
2018-07-25 23:00:17 +0200

    2 Tests

---
+++ cs/BootCamp/BootCamp/LogInSuccesFull.cs
@@ -0,0 +1,31 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace BootCamp
+{
+    [TestFixture]
+    public class LogInSuccesfull
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://techblog.polteq.com/testshop/index.php");
+
+            driver.FindElement(By.ClassName("login")).Click();
+            driver.FindElement(By.Id("email")).SendKeys("lianne.klaver@polteq.com");
+            driver.FindElement(By.Id("passwd")).SendKeys("Welkom01!");
+            driver.FindElement(By.Id("SubmitLogin")).Click();
+
+            //Console.WriteLine("SetUp is Run");
+
+            IWebElement pageHeader = driver.FindElement(By.ClassName("page-heading"));
+            Assert.AreEqual(pageHeader.Text, "MY ACCOUNT");
+
+            driver.Close();
+        }
+    }
+}
---
+++ cs/BootCamp/BootCamp/LogInSuccesFullExtended.cs
@@ -0,0 +1,41 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace BootCamp
+{
+    [TestFixture]
+    public class LogInSuccesFullExtended
+    {
+        //public IWebDriver driver = new ChromeDriver();
+        IWebDriver driver; 
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://techblog.polteq.com/testshop/index.php");
+
+            driver.FindElement(By.ClassName("login")).Click();
+            driver.FindElement(By.Id("email")).SendKeys("lianne.klaver@polteq.com");
+            driver.FindElement(By.Id("passwd")).SendKeys("Welkom01!");
+            driver.FindElement(By.Id("SubmitLogin")).Click();
+            //Console.WriteLine("SetUp is Run");
+        }
+
+        [Test]
+        public void logInPageHeaderCheck()
+        {
+
+            IWebElement pageHeader = driver.FindElement(By.ClassName("page-heading"));
+            Assert.AreEqual(pageHeader.Text, "MY ACCOUNT");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+        }
+    }
+}
