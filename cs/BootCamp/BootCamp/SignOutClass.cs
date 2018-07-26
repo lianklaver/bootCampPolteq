265bd3e84d7828f2d904a63c1738dbd4df889517
lianklaver <lianne.klaver@polteq.com>
2018-07-26 15:13:37 +0200

    Set up en Teardown

--- cs/BootCamp/BootCamp/SignOutClass.cs
+++ cs/BootCamp/BootCamp/SignOutClass.cs
@@ -8,15 +8,36 @@ using OpenQA.Selenium.Support.UI;
 namespace BootCamp
 {
     [TestFixture]
-    public class SignOutClass
+    public class TestShopScenario
     {
+        protected IWebDriver driver;
 
+        [SetUp]
+        public void Initialize()
+        {
+            //Open Chrome browser
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://techblog.polteq.com/testshop/index.php");
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void tearDown()
+        {
+            driver.Quit();
+        }
+    }
+
+    [TestFixture]
+    public class SignOutClass : TestShopScenario
+    {
+        
         [Test]
         public void SignInAndSignOutCheck()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://techblog.polteq.com/testshop/index.php");
-            driver.Manage().Window.Maximize();
+            //IWebDriver driver = new ChromeDriver();
+            //driver.Navigate().GoToUrl("https://techblog.polteq.com/testshop/index.php");
+            //driver.Manage().Window.Maximize();
 
             //Path to log in
             driver.FindElement(By.ClassName("login")).Click();
@@ -36,21 +57,21 @@ namespace BootCamp
             pageHeader = driver.FindElement(By.ClassName("page-heading"));
             Assert.AreEqual(pageHeader.Text, "AUTHENTICATION");
 
-            driver.Close();
+            //driver.Close();
         }
     }
 
     [TestFixture]
-    public class FillCartTest
+    public class FillCartTest : TestShopScenario
     {
 
         [Test]
         public void EmptyAndFilledCartCheck()
         {
             //Open Chrome browser
-            IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://techblog.polteq.com/testshop/index.php");
-            driver.Manage().Window.Maximize();
+            //IWebDriver driver = new ChromeDriver();
+            //driver.Navigate().GoToUrl("https://techblog.polteq.com/testshop/index.php");
+            //driver.Manage().Window.Maximize();
 
             //Check if cart is empty
             Assert.IsTrue(driver.FindElement(By.CssSelector("span[class='ajax_cart_no_product']")).Displayed);
@@ -66,10 +87,47 @@ namespace BootCamp
             closeButton.Click();
 
             //check 1 product in chart
-            Assert.AreEqual(driver.FindElement(By.CssSelector("span[class='ajax_cart_quantity unvisible']")).Text,"1");
+            Assert.AreEqual(driver.FindElement(By.CssSelector("span[class='ajax_cart_quantity unvisible']")).Text, "1");
             //Assert.Greater(driver.FindElement(By.CssSelector("span[class='ajax_cart_quantity unvisible']")).GetAttribute("inline")),0, "Nothing in the chart"));
 
-            driver.Close();
+            //driver.Close();
         }
     }
+
+    [TestFixture]
+    public class EmptyCartTest : TestShopScenario
+    {
+        [Test]
+        public void EmptyCartCheck()
+        {
+
+            //Open Chrome browser
+            //IWebDriver driver = new ChromeDriver();
+            //driver.Navigate().GoToUrl("https://techblog.polteq.com/testshop/index.php");
+            //driver.Manage().Window.Maximize();
+
+            //Check if cart is empty
+            Assert.IsTrue(driver.FindElement(By.CssSelector("span[class='ajax_cart_no_product']")).Displayed);
+
+            //Add Ipod Shuffel to chart
+            driver.FindElement(By.CssSelector("a[class='tag_level3 first_item']")).Click();
+            driver.FindElement(By.XPath("//a[contains(@class,'product-name')][contains(text(),'iPod shuffle')]")).Click();
+            driver.FindElement(By.CssSelector("button[class='exclusive']")).Click();
+
+            IWebElement closeButton = driver.FindElement(By.CssSelector("span[title='Close window']"));
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 2));
+            wait.Until(ExpectedConditions.ElementToBeClickable(closeButton));
+            closeButton.Click();
+
+            driver.FindElement(By.CssSelector("div[class='shopping_cart']")).Click();
+
+            //driver.FindElement(By.CssSelector("div[class='layer_cart_product col-xs-12 col-md-6']>a")).Click();
+            driver.FindElement(By.ClassName("icon-trash")).Click();
+
+            //Check if cart is empty
+            Assert.IsTrue(driver.FindElement(By.CssSelector("span[class='ajax_cart_no_product']")).Displayed);
+
+        }
+    }
+
 }
